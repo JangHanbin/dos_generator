03f2f7de69b72e88b6ba761a8374548295b23e28
janghanbin <espoirnoa09@naver.com>
2018-09-21 01:56:10 +0900

    first commit

---
+++ dosgenerator.cpp
@@ -0,0 +1,6 @@
+#include "dosgenerator.h"
+
+DosGenerator::DosGenerator()
+{
+
+}
---
+++ dosgenerator.h
@@ -0,0 +1,11 @@
+#ifndef DOSGENERATOR_H
+#define DOSGENERATOR_H
+
+
+class DosGenerator
+{
+public:
+    DosGenerator();
+};
+
+#endif // DOSGENERATOR_H
